6d69b8402f994470c4f73a4136de078af98bf031
Djavan <djatunai@gmail.com>
2025-04-15 12:28:05 -0300

    feat: add Product model, migration api routes and controller

---
+++ database/migrations/2025_04_15_150304_create_products_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->String('name');
+            $table->String('description');
+            $table->decimal('price', 8, 2);
+            $table->id();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('products');
+    }
+};
