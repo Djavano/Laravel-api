5c77bdb6a86951a865341c9dced92170ddddc255
Djavan Pereira <djatunai@gmail.com>
2025-04-15 13:30:09 -0300

    feat: implement GET all products endpoint

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -6,5 +6,6 @@ use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
-    //
+    // define quuais campos podem ser preenchidos em massa
+  protected $fillable = ['name', 'description', 'price'];
 }
